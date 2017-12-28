71af4b9801bd81f1ca593100682a4cdf397c5169
conniecas <34902892+conniecas@users.noreply.github.com>
2017-12-27 21:42:48 -0500

    Create bunnyroos.py

---
+++ bunnyroos.py
@@ -0,0 +1,46 @@
+import sys
+import time
+
+sleeping = '''                                                                                                                                                                  ^ ^                                                                                                                                                                       
+   _ _                                                                                                                                                                        {  *  }                                                                                                                                                                     
+{  \ /  }                                                                                                                                                                     {     }                                                                                                                                                                     
+   " "                                                                                                                                                                       '''
+
+awake = '''                                                                                                                                                                     ^ ^  !!!                                                                                                                                                                  
+   . .                                                                                                                                                                        {  *  }                                                                                                                                                                     
+{  \ /  }                                                                                                                                                                     {     }                                                                                                                                                                     
+   " "                                                                                                                                                                       '''
+
+string = sys.argv[1]
+
+if "bunnyroo" in string.lower():
+    print sleeping
+
+if "two" in string.lower():
+    print sleeping
+    print sleeping
+
+if "wake up" in string:
+    print sleeping
+
+if "WAKE UP" in string:
+    print awake
+
+    seconds_remaining = 3
+    while seconds_remaining > 0:
+        print " "
+        time.sleep(3)
+        seconds_remaining -= 1
+        
+    seconds_remaining = 1
+    while seconds_remaining > 0:
+        print "so sleepy ..."
+        time.sleep(1)
+        seconds_remaining -= 1
+
+    seconds_remaining = 3
+    while seconds_remaining >0:
+        print " "
+        time.sleep(1)
+        seconds_remaining -=1
+    print sleeping
